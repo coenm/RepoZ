847087dd65d22aa76a233f1910b957192ea002f8
Coen van den Munckhof <c.munckhof@gmail.com>
2022-04-25 23:26:55 +0200

    Repositories have tags which can be defined in the RepositoryActions.json. Added support for lucene search. WIP!! and to search on these tags.

---
+++ src/LuceneSearch/RepositorySearchModel.cs
@@ -0,0 +1,10 @@
+namespace LuceneSearch;
+
+internal class RepositorySearchModel
+{
+    public string RepositoryName { get; set; }
+
+    public string Path { get; set; }
+
+    public List<string> Tags { get; set; }
+}
