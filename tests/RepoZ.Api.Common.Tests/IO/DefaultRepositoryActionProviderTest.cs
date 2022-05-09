69953fa3099ff28e50c8c8e9e2d71c99c2a7c685
Coen van den Munckhof <c.munckhof@gmail.com>
2022-05-09 22:25:31 +0200

    tests

--- tests/RepoZ.Api.Common.Tests/IO/DefaultRepositoryActionProviderTest.cs
+++ tests/RepoZ.Api.Common.Tests/IO/DefaultRepositoryActionProviderTest.cs
@@ -29,47 +29,60 @@ public class DefaultRepositoryActionProviderTest
     {
         A.CallTo(() => _appDataPathProvider.GetAppDataPath()).Returns("GetAppDataPath");
         A.CallTo(() => _appDataPathProvider.GetAppResourcesPath()).Returns("GetAppResourcesPath");
+
+        A.CallTo(() => _translationService.Translate(A<string>._)).ReturnsLazily(call => call.Arguments[0] as string);
     }
 
-    // [Fact]
-    // public async Task GetPrimaryAction_ShouldReturnFirstAction_WhenConfigIsValid()
-    // {
-    //     // arrange
-    //     A.CallTo(() => _repositoryActionConfigurationStore.RepositoryActionConfiguration).Returns(new RepositoryActionConfiguration()
-    //         {
-    //             State = RepositoryActionConfiguration.LoadState.Ok,
-    //             RepositoryActions = new List<RepositoryActionConfiguration.RepositoryAction>()
-    //                 {
-    //                     new RepositoryActionConfiguration.RepositoryAction()
-    //                         {
-    //                             Active = "false",
-    //                             Name = "1-not-active",
-    //                         },
-    //                     new RepositoryActionConfiguration.RepositoryAction()
-    //                         {
-    //                             Active = "true",
-    //                             Name = "2-active",
-    //                         },
-    //                     new RepositoryActionConfiguration.RepositoryAction()
-    //                         {
-    //                             Active = "true",
-    //                             Name = "3-active",
-    //                         },
-    //                     new RepositoryActionConfiguration.RepositoryAction(),
-    //                 },
-    //         });
-    //     var sut = new DefaultRepositoryActionProvider(_repositoryActionConfigurationStore, _repositoryWriter, _repositoryMonitor, _errorHandler, _translationService);
-    //     // await using Stream stream = await EasyTestFile.LoadAsStream();
-    //     var repository = new Repository()
-    //         {
-    //             Path = "C:\\",
-    //         };
-    //
-    //     // act
-    //     RepositoryAction result = sut.GetPrimaryAction(repository);
-    //     
-    //     // assert
-    //     A.CallTo(_errorHandler).MustNotHaveHappened();
-    //     await Verifier.Verify(result);
-    // }
+    [Fact]
+    public async Task GetPrimaryAction_ShouldReturnFirstActiveAction_WhenConfigIsValid()
+    {
+        // arrange
+        A.CallTo(() => _repositoryActionConfigurationStore.RepositoryActionConfiguration).Returns(new RepositoryActionConfiguration()
+            {
+                State = RepositoryActionConfiguration.LoadState.Ok,
+                RepositoryActions = new List<RepositoryActionConfiguration.RepositoryAction>()
+                    {
+                        new RepositoryActionConfiguration.RepositoryAction()
+                            {
+                                Active = "false",
+                                Name = "1-not-active",
+                            },
+                        new RepositoryActionConfiguration.RepositoryAction()
+                            {
+                                Active = "true",
+                                Name = "2-active",
+                                Command = "browser",
+                                Arguments = "https://github.com/coenm/",
+                            },
+                        new RepositoryActionConfiguration.RepositoryAction()
+                            {
+                                Active = "true",
+                                Name = "3-active",
+                            },
+                        new RepositoryActionConfiguration.RepositoryAction(),
+                    },
+            });
+        var sut = new DefaultRepositoryActionProvider(_repositoryActionConfigurationStore, _repositoryWriter, _repositoryMonitor, _errorHandler, _translationService);
+        // await using Stream stream = await EasyTestFile.LoadAsStream();
+        var repository = new Repository()
+            {
+                Path = "C:\\",
+                Branches = new []{ "develop", "main", },
+                LocalBranches = new[] { "develop", },
+                RemoteUrls = new []{ "https://github.com/coenm/RepoZ.git", },
+        };
+    
+        // act
+        RepositoryAction result = sut.GetPrimaryAction(repository);
+        
+        // assert
+        A.CallTo(_errorHandler).MustNotHaveHappened();
+        await Verifier.Verify(new
+            {
+                result.BeginGroup,
+                result.Name,
+                result.CanExecute,
+                result.ExecutionCausesSynchronizing,
+            }).ModifySerialization(s => s.DontIgnoreFalse());
+    }
 }
